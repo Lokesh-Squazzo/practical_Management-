28d9ec09a57f46981c7845527ff169516db8c7d5
Lokesh-Squazzo <lokeshgamingclub@gmail.com>
2024-11-28 17:25:36 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,5 +19,5 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("DB connection error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
